303f95821aa6b2608b18e3ff88557626408c45f9
Erick Solano <erick.solanog@gmail.com>
2023-02-20 16:38:03 -0600

    Project Planner - Vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,62 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div v-if="projects.length">
+      <FilterNav @filter="current = $event" :current="current" />
+      <div v-for="project in this.filteredProjects" :key="project.id">
+        <SingleProject
+          :project="project"
+          @delete="handleDelete"
+          @complete="handleComplete"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import FilterNav from "@/components/FilterNav.vue";
+import SingleProject from "@/components/SingleProject.vue";
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'HomeView',
+  name: "HomeView",
   components: {
-    HelloWorld
-  }
-}
+    FilterNav,
+    SingleProject,
+  },
+  data() {
+    return {
+      projects: [],
+      current: "all",
+    };
+  },
+  computed: {
+    filteredProjects() {
+      if (this.current === "completed") {
+        return this.projects.filter((x) => x.complete);
+      }
+
+      if (this.current === "ongoing") {
+        return this.projects.filter((x) => !x.complete);
+      }
+
+      return this.projects;
+    },
+  },
+  methods: {
+    handleDelete(id) {
+      this.projects = this.projects.filter((p) => p.id !== id);
+    },
+    handleComplete(id) {
+      let project = this.projects.find((p) => p.id === id);
+      project.complete = !project.complete;
+    },
+  },
+  mounted() {
+    fetch("http://localhost:3000/projects")
+      .then((res) => res.json())
+      .then((data) => (this.projects = data))
+      .catch((err) => console.log(err.message));
+  },
+};
 </script>
